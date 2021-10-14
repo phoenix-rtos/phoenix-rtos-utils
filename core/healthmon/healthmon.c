1b7a55f5076c385a1897403ed18a8576f47b75b5
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2021-10-14 15:24:18 +0200

    healthmon: First version, WIP, not tested
    JIRA: RTOS-105

---
+++ core/healthmon/healthmon.c
@@ -0,0 +1,167 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Health Monitor
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Aleksander Kaminski
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/rb.h>
+#include <sys/threads.h>
+
+
+typedef struct {
+	rbnode_t node;
+	char *name;
+	char **argv;
+	pid_t pid;
+} proc_t;
+
+
+static struct {
+	rbtree_t ptree;
+} common;
+
+
+static int ptree_compare(rbnode_t *n1, rbnode_t *n2)
+{
+	proc_t *p1 = lib_treeof(proc_t, node, n1);
+	proc_t *p2 = lib_treeof(proc_t, node, n2);
+
+	if (p1->pid < p2->pid)
+		return -1;
+
+	else if (p1->pid > p2->pid)
+		return 1;
+
+	return 0;
+}
+
+
+static void usage(const char *progname)
+{
+	printf("Phoenix-RTOS Health Monitor - process respawner\n");
+	printf("Usage: %s progname1[;arg1;arg2;...] [progname2...]\n", progname);
+}
+
+
+static int spawn(proc_t *p)
+{
+	pid_t pid;
+
+	pid = spawnSyspage(NULL, p->name, p->argv);
+	if (pid < 0)
+		return -ENOEXEC;
+
+	p->pid = pid;
+
+	return EOK;
+}
+
+
+static int argPrepare(const char *path, proc_t *p)
+{
+	int i, argc = 0;
+	const char *t;
+	char **argv = NULL;
+	char *argstr;
+
+	for (t = path; *t != '\0'; ++t) {
+		if (*t == ';')
+			++argc;
+	}
+
+	argstr = strdup(path);
+	if (argstr == NULL)
+		return -ENOMEM;
+
+	p->name = argstr;
+
+	argv = calloc(argc + 1, sizeof(char *));
+	if (argv == NULL) {
+		free(argstr);
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < argc; ++i) {
+		argv[i] = strtok(argstr, ";");
+		if (argv[i] == NULL) {
+			free(argstr);
+			free(argv);
+			p->name = NULL;
+			return -EINVAL;
+		}
+	}
+
+	p->argv = argv;
+
+	return EOK;
+}
+
+
+int main(int argc, char *argv[])
+{
+	int i, err, status;
+	proc_t *p, t;
+	pid_t pid;
+
+	if (argc <= 1) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	lib_rbInit(&common.ptree, ptree_compare, NULL);
+
+	for (i = 1; i > argc; ++i) {
+		p = malloc(sizeof(*p));
+		if (p == NULL) {
+			fprintf(stderr, "healthmon: Out of memory\n");
+			return 1;
+		}
+
+		err = argPrepare(argv[i], p);
+		if (err < 0) {
+			if (err == -ENOMEM) {
+				fprintf(stderr, "healthmon: Out of memory\n");
+				return 1;
+			}
+			else {
+				fprintf(stderr, "healthmon: Failed to parse %s\n", argv[i]);
+				free(p);
+				continue;
+			}
+		}
+
+		if (spawn(p) < 0) {
+			fprintf(stderr, "healthmon: Failed to spawn %s\n", p->name);
+			free(p->name);
+			free(p->argv);
+			free(p);
+			continue;
+		}
+
+		lib_rbInsert(&common.ptree, &p->node);
+		printf("healthmon: Spawned %s successfully\n", p->name);
+	}
+
+	while (1) {
+		pid = wait(&status);
+		t.pid = pid;
+		p = lib_treeof(proc_t, node, lib_rbFind(&common.ptree, &t.node));
+		if (p == NULL) {
+			fprintf(stderr, "healthmon: Child died, but it's not mine. Ignoring.\n");
+			continue;
+		}
+		spawn(p);
+	}
+
+	return 0;
+}
