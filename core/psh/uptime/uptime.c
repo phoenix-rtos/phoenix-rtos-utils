49e55c2f49efafe2f661138394c130208cbdd619
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2021-10-19 19:07:19 +0200

    psh: make uptime more human readable
    Make uptime return by default more human readable information like
    "up N days and HH:MM:SS", instead of only the seconds count from
    system start.
    
    JIRA: RTOS-108

--- core/psh/uptime/uptime.c
+++ core/psh/uptime/uptime.c
@@ -4,7 +4,7 @@
  * uptime - prints how long the system has been running
  *
  * Copyright 2021 Phoenix Systems
- * Author: Ziemowit Leszczynski
+ * Author: Ziemowit Leszczynski, Gerard Swiderski
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -27,12 +27,37 @@ void psh_uptimeinfo(void)
 int psh_uptime(int argc, char **argv)
 {
 	struct timespec tp;
-	int rv = clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
+	intmax_t days, seconds;
+	int minutes, hours, rv;
+
+	rv = clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
 	if (rv < 0) {
 		fprintf(stderr, "uptime: failed to get time\n");
 		return -EINVAL;
 	}
-	printf("%jd\n", (intmax_t)tp.tv_sec);
+
+	seconds = tp.tv_sec;
+
+	if (argc == 1 && seconds >= 0) {
+		days = seconds / 86400;
+		seconds %= 86400;
+		hours = seconds / 3600;
+		seconds %= 3600;
+		minutes = seconds / 60;
+		seconds %= 60;
+
+		printf("up ");
+
+		if (days > 0)
+			printf("%jd day%s and ", days, days == 1 ? "" : "s");
+
+		printf("%02d:%02d:%02d\n", hours, minutes, (int)seconds);
+	}
+	else {
+		printf("%jd\n", seconds);
+	}
+
+
 	return EOK;
 }
 
