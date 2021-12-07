48a626adedadba9f3aca407e4ae5d6521f4872ec
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2021-12-07 17:53:38 +0100

    psh: reboot: add reboot reason retrieval
    JIRA: DTR-181

--- core/psh/reboot/reboot.c
+++ core/psh/reboot/reboot.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <stdint.h>
 #include <sys/reboot.h>
 
 #include "../psh.h"
@@ -26,27 +27,60 @@ static void psh_rebootinfo(void)
 }
 
 
+static void print_help(const char *progname)
+{
+	printf("Usage: %s [options] address\n", progname);
+	printf("Options\n");
+	printf("  -s:  reboot to secondary boot option\n");
+	printf("  -g:  get bootreason (platform-specific)\n");
+	printf("  -h:  show help\n");
+}
+
+
 static int psh_reboot(int argc, char **argv)
 {
 	int c, magic = PHOENIX_REBOOT_MAGIC;
+	int op_get = 0;
+	int op_secondary = 1;
 
-	while ((c = getopt(argc, argv, "s")) != -1) {
+	while ((c = getopt(argc, argv, "ghs")) != -1) {
 		switch (c) {
-		case 's':
-			magic = ~magic;
-			break;
+			case 's':
+				op_secondary = 1;
+				break;
 
-		default:
-			return EOK;
+			case 'h':
+				print_help(argv[0]);
+				return 0;
+
+			case 'g':
+				op_get = 1;
+				break;
+
+			default:
+				print_help(argv[0]);
+				return 1;
 		}
 	}
 
-	if (reboot(magic) < 0) {
-		fprintf(stderr, "reboot: failed to restart the machine\n");
-		return -EPERM;
+	if (op_secondary)
+		magic = ~magic;
+
+	if (op_get) {
+		uint32_t reason;
+		if (reboot_reason(&reason) < 0)
+			return 1;
+
+		printf("0x%08x\n", reason);
+	}
+	else {
+		if (reboot(magic) < 0) {
+			fprintf(stderr, "reboot: failed to restart the machine\n");
+			return 1;
+		}
 	}
 
-	return EOK;
+	return 0;
 }
 
 
