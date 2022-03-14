396bef6b1c0df85096520c8462bf63ed24b0c207
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2022-03-14 18:36:46 +0100

    psh/ps/top: fix WAIT column formatting + add more space for PID and PPID columns
    JIRA: RTOS-166

--- psh/ps/ps.c
+++ psh/ps/ps.c
@@ -136,10 +136,10 @@ static int psh_ps(int argc, char **argv)
 				tcnt -= j - i - 1;
 			}
 		}
-		printf("%5s %5s %2s %5s %5s %5s %7s %6s %3s %-28s\n", "PID", "PPID", "PR", "STATE", "%CPU", "WAIT", "TIME", "VMEM", "THR", "CMD");
+		printf("%8s %8s %2s %5s %5s %6s %7s %6s %3s %-21s\n", "PID", "PPID", "PR", "STATE", "%CPU", "WAIT", "TIME", "VMEM", "THR", "CMD");
 	}
 	else {
-		printf("%5s %5s %2s %5s %5s %5s %7s %6s %-32s\n", "PID", "PPID", "PR", "STATE", "%CPU", "WAIT", "TIME", "VMEM", "CMD");
+		printf("%8s %8s %2s %5s %5s %6s %7s %6s %-25s\n", "PID", "PPID", "PR", "STATE", "%CPU", "WAIT", "TIME", "VMEM", "CMD");
 	}
 
 	qsort(info, tcnt, sizeof(threadinfo_t), cmp);
@@ -149,16 +149,16 @@ static int psh_ps(int argc, char **argv)
 		h = info[i].cpuTime / 3600;
 		m = (info[i].cpuTime - h * 3600) / 60;
 		psh_prefix(10, info[i].wait, -6, 1, buff);
-		printf("%5u %5u %2d %5s %3u.%u %4ss %4u:%02u ", info[i].pid, info[i].ppid, info[i].priority, (info[i].state) ? "sleep" : "ready",
+		printf("%8u %8u %2d %5s %3u.%u %5ss %4u:%02u ", info[i].pid, info[i].ppid, info[i].priority, (info[i].state) ? "sleep" : "ready",
 			info[i].load / 10, info[i].load % 10, buff, h, m);
 
 		psh_prefix(2, info[i].vmem, 0, 1, buff);
 		printf("%6s ", buff);
 
 		if (!threads)
-			printf("%3u %.*s\n", info[i].tid, (int)(fullcmd ? sizeof(info[i].name) : 28), info[i].name);
+			printf("%3u %.*s\n", info[i].tid, (int)(fullcmd ? sizeof(info[i].name) : 21), info[i].name);
 		else
-			printf("%.*s\n", (int)(fullcmd ? sizeof(info[i].name) : 32), info[i].name);
+			printf("%.*s\n", (int)(fullcmd ? sizeof(info[i].name) : 25), info[i].name);
 	}
 
 	free(info);
