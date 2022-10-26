86c1dcacbb7dad3779bab4b4f4f9b04a0b07581b
Maciej Purski <maciej.purski@phoenix-rtos.com>
2022-10-26 17:16:15 +0200

    nandtool: Add dumping functionality
    JIRA: DTR-351

--- nandtool/nandtool.c
+++ nandtool/nandtool.c
@@ -169,7 +169,7 @@ static int nandtool_check(void)
 	int err;
 
 	if ((err = flashmng_checkbad(nandtool_common.oid)) < 0) {
-		fprintf(stderr, "nandtool: failed to complete bad block check, err: %d\n", err);
+		fprintf(stderr, "nandtool: failed to complete bad block check, %d\n", err);
 		return err;
 	}
 
@@ -177,13 +177,116 @@ static int nandtool_check(void)
 }
 
 
+static int nandtool_dump(const char *outpath, unsigned int start, unsigned int nblocks, int oob)
+{
+	const flashsrv_info_t *info;
+	ssize_t len;
+	offs_t blocksz, partsz, pagesz;
+	offs_t addr, endaddr, bytes;
+	uint8_t *buf;
+	int ret = 0;
+	int outfd;
+
+	outfd = open(outpath, O_WRONLY | O_CREAT);
+	if (outfd < 0) {
+		perror("nandtool_dump: Fail to open output file");
+		return -EINVAL;
+	}
+
+	info = flashmng_info(nandtool_common.oid);
+	if (info == NULL) {
+		fprintf(stderr, "nandtool_dump: Fail to retrieve partition info\n");
+		close(outfd);
+		return -EFAULT;
+	}
+
+	if (!oob) {
+		pagesz = info->writesz;
+		blocksz = info->erasesz;
+		partsz = info->size;
+	}
+	else {
+		pagesz = info->metasz + info->writesz;
+		blocksz = (info->erasesz / info->writesz) * pagesz;
+		partsz = (info->size / info->erasesz) * blocksz;
+	}
+
+	addr = start * blocksz;
+
+	if (!oob) {
+		if (lseek(nandtool_common.fd, addr, SEEK_SET) < 0) {
+			perror("nandtool_dump: lseek failed");
+			close(outfd);
+			return -EINVAL;
+		}
+	}
+
+	buf = malloc(pagesz);
+	if (buf == NULL) {
+		perror("nandtool_dump: Fail to allocate memory");
+		close(outfd);
+		return -ENOMEM;
+	}
+
+	if (nblocks == 0) {
+		/* Read everything from start to end of partition */
+		endaddr = partsz;
+	}
+	else {
+		endaddr = addr + nblocks * blocksz;
+	}
+
+	printf("nandtool_dump: %spartition size: %llu, erase block size: %d, write size: %d\n",
+		(oob == 1) ? "raw " : "", partsz, info->erasesz, info->writesz);
+
+	printf("nandtool_dump: Reading from address: 0x%llx to 0x%llx\n", addr, endaddr);
+
+	bytes = 0;
+	while (addr + bytes < endaddr) {
+		if (oob) {
+			len = flashmng_readraw(nandtool_common.oid, addr + bytes, buf, pagesz);
+		}
+		else {
+			len = read(nandtool_common.fd, buf, pagesz);
+		}
+
+		if (len != pagesz) {
+			fprintf(stderr, "nandtool_dump: Fail to read a block at offset: %llx, %s\n", addr + bytes, strerror(errno));
+			ret = -EIO;
+			break;
+		}
+
+		len = write(outfd, buf, pagesz);
+		if (len != pagesz) {
+			fprintf(stderr, "nandtool_dump: Fail to write to output file at offset: %llx, %s\n", bytes, strerror(errno));
+			ret = -EIO;
+			break;
+		}
+
+		bytes += len;
+	}
+
+	printf("nandtool_dump: Written %lld bytes to %s file\n", bytes, outpath);
+
+	close(outfd);
+	free(buf);
+
+	return ret;
+}
+
+
 static void nandtool_help(const char *prog)
 {
 	printf("Usage: %s [options] <device>\n", prog);
-	printf("\t-e <start[:size]> - erase size block(s) starting from start\n");
+	printf("\t-e <start[:size]> - erase size block(s) starting from start or from the beginning if start arg is not provided\n");
 	printf("\t                    (skip size to erase one block or pass size=0 to erase whole device)\n");
 	printf("\t-j                - write jffs2 cleanmarkers (valid only with erase operation)\n");
 	printf("\t-c                - check device for bad blocks and print summary\n");
+	printf("\n");
+	printf("\t-d <start[:size]> - dump size blocks starting from start or from the beginning if start arg is not provided, requires -o\n");
+	printf("\t                    (skip size to dump one block or pass size=0 to dump everything until the end of a partition)\n");
+	printf("\t-o <path>         - path of the file to dump data\n");
+	printf("\t-b                - dump out-of-bound data (raw reads)\n");
 	printf("\n");
 	printf("\t-i <path>         - path of the file to flash (requires -s option)\n");
 	printf("\t-r                - flash raw data\n");
@@ -193,30 +296,71 @@ static void nandtool_help(const char *prog)
 }
 
 
+static int nandtool_parseRange(const char *str, int *start, int *size)
+{
+	char *endptr;
+
+	*start = strtoul(str, &endptr, 0);
+	if (*endptr == '\0') {
+		/* Default case, one block */
+		*size = 1;
+	}
+	else if (*endptr == ':') {
+		str = endptr + 1;
+		*size = strtoul(str, &endptr, 0);
+		if ((str == endptr) || (*endptr != '\0')) {
+			return -1;
+		}
+	}
+	else {
+		return -1;
+	}
+
+	return 0;
+}
+
+
 int main(int argc, char **argv)
 {
 	int check = 0, raw = 0, flash_start = -1, erase_start = -1, erase_size = -1, write_cleanmarkers = 0;
-	char *dev, *tok, *path = NULL;
+	int dump_start = -1, dump_size = -1, oob = 0;
+	const char *ipath = NULL, *opath = NULL;
+	char *dev;
 	int c, err;
 
 	if (isatty(STDOUT_FILENO))
 		nandtool_common.interactive = 1;
 
-	while ((c = getopt(argc, argv, "e:i:r:s:chjq")) != -1) {
+	while ((c = getopt(argc, argv, "e:d:i:o:rs:bchjq")) != -1) {
 		switch (c) {
 			case 'e':
-				tok = strtok(optarg, ":");
-				erase_start = strtoul(tok, NULL, 0);
-				erase_size = ((tok = strtok(NULL, ":")) == NULL) ? 1 : strtoul(tok, NULL, 0);
+				if (nandtool_parseRange(optarg, &erase_start, &erase_size) < 0) {
+					fprintf(stderr, "nandtool: Fail to parse erase args!\n");
+					return 1;
+				}
+				break;
+
+			case 'd':
+				if (nandtool_parseRange(optarg, &dump_start, &dump_size) < 0) {
+					fprintf(stderr, "nandtool: Fail to parse dump args!\n");
+					return 1;
+				}
 				break;
 
 			case 'j':
 				write_cleanmarkers = 1;
 				break;
 
+			case 'o':
+				opath = optarg;
+				break;
+
 			case 'i':
-				path = optarg;
-				raw = 0;
+				ipath = optarg;
+				break;
+
+			case 'b':
+				oob = 1;
 				break;
 
 			case 'r':
@@ -282,9 +426,13 @@ int main(int argc, char **argv)
 		if ((erase_start >= 0) && (erase_size >= 0) && ((err = nandtool_erase(erase_start, erase_size, write_cleanmarkers)) < 0))
 			break;
 
-		if ((flash_start >= 0) && (path != NULL) && ((err = nandtool_flash(path, flash_start, raw)) < 0))
+		if ((flash_start >= 0) && (ipath != NULL) && ((err = nandtool_flash(ipath, flash_start, raw)) < 0))
 			break;
 	} while (0);
+
+	if ((dump_start >= 0) && (opath != NULL)) {
+		nandtool_dump(opath, dump_start, dump_size, oob);
+	}
 
 	close(nandtool_common.fd);
 
