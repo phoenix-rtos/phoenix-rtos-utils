9b8b80d5b5883e3e6cc94bed9b1f25bbe8137230
Gerard Swiderski <gerard@swiderski.eu>
2023-06-20 20:09:41 +0200

    psh/dd: Add minimal conv support
    Extends `dd` tool with `conv` a comma separated symbol list.
    Curently supported convert parameters are:
     * `notrunc` - do not truncate the output file,
     * `nocreat` - do not create the output file.
    
    JIRA: RTOS-399

--- psh/dd/dd.c
+++ psh/dd/dd.c
@@ -22,11 +22,8 @@
 
 #include "../psh.h"
 
+
 /* clang-format off */
-
-enum { param_none = 0, param_if, param_of, param_bs,
-	param_count, param_seek, param_skip };
-
 
 struct param_s {
 	char *name;
@@ -34,14 +31,29 @@ struct param_s {
 };
 
 
+enum { param_none = 0, param_if, param_of, param_bs,
+	param_conv, param_count, param_seek, param_skip };
+
+
 static const struct param_s params[] = {
 	{ "if", param_if },
 	{ "of", param_of },
 	{ "bs", param_bs },
+	{ "conv", param_conv },
 	{ "count", param_count },
 	{ "seek", param_seek },
 	{ "skip", param_skip },
 	{ NULL, param_none }
+};
+
+
+enum { conv_none = 0, conv_nocreat, conv_notrunc };
+
+
+static const struct param_s convs[] = {
+	{ "nocreat", conv_nocreat },
+	{ "notrunc", conv_notrunc },
+	{ NULL, conv_none }
 };
 
 /* clang-format on */
@@ -57,16 +69,18 @@ static void psh_dd_usage(void)
 {
 	printf(
 		"Usage: dd [OPERAND]...\n"
-		"\tif=FILE   read from FILE instead of stdin\n"
-		"\tof=FILE   write to FILE instead of stdout\n"
-		"\tbs=BYTE   read/write block size of BYTES bytes\n"
-		"\tcount=N   copy only N input blocks\n"
-		"\tseek=N    skip N bs-sized blocks at start of output\n"
-		"\tskip=N    skip N bs-sized blocks at start of input\n");
+		"\tif=FILE     read from FILE instead of stdin\n"
+		"\tof=FILE     write to FILE instead of stdout\n"
+		"\tbs=BYTE     read/write block size of BYTES bytes\n"
+		"\tcount=N     copy only N input blocks\n"
+		"\tseek=N      skip N bs-sized blocks at start of output\n"
+		"\tskip=N      skip N bs-sized blocks at start of input\n"
+		"\tconv=CONVS  comma-separated list of supported conversions:\n"
+		"\t            e.g. nocreat,notrunc\n");
 }
 
 
-static ssize_t getnumber(char *numstr)
+static ssize_t getnumber(const char *numstr)
 {
 	ssize_t value;
 
@@ -115,15 +129,56 @@ static ssize_t getnumber(char *numstr)
 }
 
 
+static int getconvmode(mode_t *m, const char *str)
+{
+	const char *end;
+	const struct param_s *par;
+	mode_t mode = *m;
+
+	for (end = str; *end != '\0'; str = end + 1) {
+		end = strchr(str, ',');
+		if (end == NULL) {
+			end = str + strlen(str);
+		}
+
+		for (par = convs; par->name != NULL; par++) {
+			size_t len = end - str;
+			if ((strncmp(str, par->name, len) == 0) && (par->name[len] == '\0')) {
+				break;
+			}
+		}
+
+		switch (par->value) {
+			case conv_nocreat:
+				mode &= ~O_CREAT;
+				break;
+
+			case conv_notrunc:
+				mode &= ~O_TRUNC;
+				break;
+
+			default:
+				return -EINVAL;
+		}
+	}
+
+	*m = mode;
+
+	return EOK;
+}
+
+
 static int psh_dd(int argc, char **argv)
 {
 	const struct param_s *par;
-	char *buf, *str, *cp;
+	const char *str, *cp;
+	char *buf, *p;
 	int infd, outfd, incc, outcc;
 	ssize_t intotal, outtotal;
 
-	char *infile = NULL;
-	char *outfile = NULL;
+	const char *infile = NULL;
+	const char *outfile = NULL;
+	mode_t outmode = O_CREAT | O_TRUNC;
 	ssize_t count = SSIZE_MAX;
 	ssize_t seekval = 0;
 	ssize_t skipval = 0;
@@ -143,8 +198,9 @@ static int psh_dd(int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 
-		for (par = params; par->name; par++) {
-			if (strncmp(str, par->name, cp - str) == 0) {
+		for (par = params; par->name != NULL; par++) {
+			size_t len = cp - str;
+			if ((strncmp(str, par->name, len) == 0) && (par->name[len] == '\0')) {
 				break;
 			}
 		}
@@ -172,6 +228,13 @@ static int psh_dd(int argc, char **argv)
 				blocksz = getnumber(cp);
 				if (blocksz <= 0) {
 					fprintf(stderr, "Bad block size value\n");
+					return EXIT_FAILURE;
+				}
+				break;
+
+			case param_conv:
+				if (getconvmode(&outmode, cp) < 0) {
+					fprintf(stderr, "Invalid conv symbol list\n");
 					return EXIT_FAILURE;
 				}
 				break;
@@ -222,7 +285,7 @@ static int psh_dd(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	outfd = ((outfile == NULL) ? fileno(stdout) : creat(outfile, 0666));
+	outfd = ((outfile == NULL) ? fileno(stdout) : open(outfile, outmode | O_WRONLY, 0666));
 	if (outfd < 0) {
 		fprintf(stderr, "'%s': %s\n", outfile, strerror(errno));
 		if (infd != fileno(stdin)) {
@@ -269,35 +332,42 @@ static int psh_dd(int argc, char **argv)
 		for (;;) {
 			incc = read(infd, buf, blocksz);
 			if (incc <= 0) {
+				if ((incc < 0) && (errno == EINTR)) {
+					continue;
+				}
+
 				break;
 			}
 
 			intotal += incc;
-			cp = buf;
+			p = buf;
 
-			if (psh_common.sigint) {
+			if (psh_common.sigint != 0) {
 				fprintf(stderr, "Interrupted\n");
 				err = 1;
 				break; /* goto cleanup */
 			}
 
 			while (incc > 0) {
-				outcc = write(outfd, cp, incc);
+				outcc = write(outfd, p, incc);
 				if (outcc < 0) {
+					if (errno == EINTR) {
+						continue;
+					}
 					perror(outfile);
 					err = 1;
 					break; /* goto cleanup */
 				}
 				outtotal += outcc;
-				cp += outcc;
 				incc -= outcc;
+				p += outcc;
 			}
 
 			if (err == 1) {
 				break;
 			}
 
-			if (inmax && (intotal >= inmax)) {
+			if ((inmax != 0) && (intotal >= inmax)) {
 				break;
 			}
 		}
