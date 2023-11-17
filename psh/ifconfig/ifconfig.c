0ef89121ccc9a07ce32ac61081c2bb655cc271bd
Andrzej Stalke <andrzej.stalke@phoenix-rtos.com>
2023-11-17 17:32:09 +0100

    psh/ifconfig: Add configuration commands
    JIRA: RTOS-628

--- psh/ifconfig/ifconfig.c
+++ psh/ifconfig/ifconfig.c
@@ -27,6 +27,8 @@
 #define IFCONFIG_VERBOSE 1
 #define IFCONFIG_ALL     2
 
+enum operation {operation_setFlag, operation_unsetFlag, operation_toggleFlag};
+
 static void psh_ifconfigInfo(void)
 {
 	printf("configures network interfaces");
@@ -81,7 +83,7 @@ static inline int psh_ifconfigPrintInterface(const struct ifaddrs *interface, in
 	int ret;
 	struct ifreq ioctlInterface;
 
-	(void)memcpy(ioctlInterface.ifr_name, interface->ifa_name, strlen(interface->ifa_name));
+	(void)strncpy(ioctlInterface.ifr_name, interface->ifa_name, IFNAMSIZ - 1);
 	/* Get interface flags */
 	ret = ioctl(sd, SIOCGIFFLAGS, &ioctlInterface);
 	if (ret < 0) {
@@ -162,10 +164,10 @@ static inline int psh_ifconfigPrintInterface(const struct ifaddrs *interface, in
 }
 
 
-static int psh_ifconfigDisplay(unsigned int flags, const char *interfaceName)
+static inline int psh_ifconfigDisplay(unsigned int flags, const char *interfaceName, int sd)
 {
 	struct ifaddrs *interface;
-	int ret, sd;
+	int ret;
 
 	ret = getifaddrs(&interface);
 	if (ret != 0) {
@@ -173,15 +175,9 @@ static int psh_ifconfigDisplay(unsigned int flags, const char *interfaceName)
 		return ret;
 	}
 
-	sd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sd < 0) {
-		perror("socket");
-		return sd;
-	}
-
 	for (; interface != NULL; interface = interface->ifa_next) {
 		if ((interfaceName == NULL) || (strcmp(interfaceName, interface->ifa_name) == 0)) {
-			if (((flags & IFCONFIG_ALL) != 0) || ((interface->ifa_flags & IFF_UP) != 0)) {
+			if (((flags & IFCONFIG_ALL) != 0) || (((interface->ifa_flags & IFF_UP) != 0)) || (interfaceName != NULL)) {
 				ret = psh_ifconfigPrintInterface(interface, sd);
 				if (ret < 0) {
 					break;
@@ -189,8 +185,261 @@ static int psh_ifconfigDisplay(unsigned int flags, const char *interfaceName)
 			}
 		}
 	}
-	close(sd);
 	freeifaddrs(interface);
+	return ret;
+}
+
+
+static int psh_ifconfigChangeFlag(struct ifreq *ioctlInterface, int sd, unsigned int flag, enum operation op)
+{
+	int ret;
+	ret = ioctl(sd, SIOCGIFFLAGS, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCGIFFLAGS)");
+		return ret;
+	}
+	switch (op) {
+		case operation_setFlag:
+			ioctlInterface->ifr_flags |= flag;
+			break;
+		case operation_unsetFlag:
+			ioctlInterface->ifr_flags &= ~flag;
+			break;
+		case operation_toggleFlag:
+			ioctlInterface->ifr_flags ^= flag;
+			break;
+		default:
+			/* Programming error */
+			return -1;
+	}
+
+	ret = ioctl(sd, SIOCSIFFLAGS, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCSIFFLAGS)");
+		return ret;
+	}
+	return 0;
+}
+
+
+static int psh_ifconfigUpHandler(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_UP, operation_setFlag);
+}
+
+
+static int psh_ifconfigDownHandler(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_UP, operation_unsetFlag);
+}
+
+static int psh_ifconfigNetmask(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	int ret;
+
+	if (*opt >= argc) {
+		/* Not enough arguments */
+		fprintf(stderr, "Not enough arguments\n");
+		return -1;
+	}
+
+	(*opt) += 1;
+	((struct sockaddr_in *)&ioctlInterface->ifr_netmask)->sin_family = AF_INET;
+	ret = inet_aton(argv[*opt], &((struct sockaddr_in *)&ioctlInterface->ifr_netmask)->sin_addr);
+	if (ret == 0) {
+		fprintf(stderr, "Invalid netmask value\n");
+		return -1;
+	}
+	ret = ioctl(sd, SIOCSIFNETMASK, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCSIFNETMASK)");
+		return ret;
+	}
+	return 0;
+}
+
+
+static int psh_ifconfigBroadcast(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	int ret;
+
+	if (*opt >= argc) {
+		fprintf(stderr, "Not enough arguments\n");
+		return -1;
+	}
+
+	(*opt) += 1;
+	((struct sockaddr_in *)&ioctlInterface->ifr_broadaddr)->sin_family = AF_INET;
+	ret = inet_aton(argv[*opt], &((struct sockaddr_in *)&ioctlInterface->ifr_broadaddr)->sin_addr);
+	if (ret == 0) {
+		fprintf(stderr, "Invalid broadcast value\n");
+		return -1;
+	}
+	ret = ioctl(sd, SIOCSIFBRDADDR, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCSIFBRDADDR)");
+		return ret;
+	}
+	return 0;
+}
+
+
+static int psh_ifconfigMTU(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	int ret;
+	char *endptr;
+
+	if (*opt >= argc) {
+		fprintf(stderr, "Not enough arguments\n");
+		return -1;
+	}
+
+	(*opt) += 1;
+	ioctlInterface->ifr_mtu = strtol(argv[*opt], &endptr, 0);
+	if (*endptr != '\0') {
+		fprintf(stderr, "Invalid MTU value\n");
+		return -1;
+	}
+	ret = ioctl(sd, SIOCSIFMTU, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCSIFMTU)");
+		return ret;
+	}
+	return 0;
+}
+
+
+static int psh_ifconfigPointToPoint(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	int ret;
+
+	if (*opt >= argc) {
+		fprintf(stderr, "Not enough arguments\n");
+		return -1;
+	}
+
+	(*opt) += 1;
+	((struct sockaddr_in *)&ioctlInterface->ifr_dstaddr)->sin_family = AF_INET;
+	ret = inet_aton(argv[*opt], &((struct sockaddr_in *)&ioctlInterface->ifr_dstaddr)->sin_addr);
+	if (ret == 0) {
+		fprintf(stderr, "Invalid point-to-point address value\n");
+		return -1;
+	}
+	ret = ioctl(sd, SIOCSIFDSTADDR, ioctlInterface);
+	if (ret != 0) {
+		perror("ioctl(SIOCSIFDSTADDR)");
+		return ret;
+	}
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_POINTOPOINT, operation_setFlag);
+}
+
+
+static int psh_ifconfigMulticast(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_MULTICAST, operation_toggleFlag);
+}
+
+
+static int psh_ifconfigAllmulti(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_ALLMULTI, operation_toggleFlag);
+}
+
+
+static int psh_ifconfigPromisc(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_PROMISC, operation_toggleFlag);
+}
+
+
+static int psh_ifconfigArp(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt)
+{
+	(void)argc;
+	(void)argv;
+	(void)opt;
+	return psh_ifconfigChangeFlag(ioctlInterface, sd, IFF_NOARP, operation_toggleFlag);
+}
+
+
+static const struct {
+	const char *name;
+	int (*handler)(struct ifreq *ioctlInterface, int sd, int argc, char **argv, int *opt);
+} psh_ifconfigArguments[] = {
+	{ .name = "up", .handler = psh_ifconfigUpHandler },
+	{ .name = "down", .handler = psh_ifconfigDownHandler },
+	{ .name = "netmask", .handler = psh_ifconfigNetmask },
+	{ .name = "broadcast", .handler = psh_ifconfigBroadcast },
+	{ .name = "-broadcast", .handler = psh_ifconfigBroadcast },
+	{ .name = "mtu", .handler = psh_ifconfigMTU },
+	{ .name = "dstaddr", .handler = psh_ifconfigPointToPoint },
+	{ .name = "pointopoint", .handler = psh_ifconfigPointToPoint },
+	{ .name = "-pointopoint", .handler = psh_ifconfigPointToPoint },
+	{ .name = "multicast", .handler = psh_ifconfigMulticast },
+	{ .name = "allmulti", .handler = psh_ifconfigAllmulti },
+	{ .name = "-allmulti", .handler = psh_ifconfigAllmulti },
+	{ .name = "promisc", .handler = psh_ifconfigPromisc },
+	{ .name = "-promisc", .handler = psh_ifconfigPromisc },
+	{ .name = "arp", .handler = psh_ifconfigArp },
+	{ .name = "-arp", .handler = psh_ifconfigArp },
+};
+
+
+static inline int psh_ifconfigHandleArguments(const char *interfaceName, int argc, char **argv, int opt, int sd)
+{
+	int ret, found;
+	size_t i;
+	struct ifreq ioctlInterface;
+	const char *argumentName;
+
+	for (; opt < argc; opt++) {
+		(void)strncpy(ioctlInterface.ifr_name, interfaceName, IFNAMSIZ - 1);
+		(void)memset(&ioctlInterface.ifr_ifru, 0, sizeof(ioctlInterface.ifr_ifru));
+		argumentName = argv[opt];
+		ret = 1;
+		found = 0;
+		for (i = 0; i < sizeof(psh_ifconfigArguments) / sizeof(*psh_ifconfigArguments); ++i) {
+			if (strcmp(argumentName, psh_ifconfigArguments[i].name) == 0) {
+				found = 1;
+				ret = psh_ifconfigArguments[i].handler(&ioctlInterface, sd, argc, argv, &opt);
+				break;
+			}
+		}
+		if (found == 0) {
+			(void)strncpy(ioctlInterface.ifr_name, interfaceName, IFNAMSIZ - 1);
+			(void)memset(&ioctlInterface.ifr_ifru, 0, sizeof(ioctlInterface.ifr_ifru));
+			/* Is this an IP address? */
+			((struct sockaddr_in *)&ioctlInterface.ifr_netmask)->sin_family = AF_INET;
+			ret = inet_aton(argumentName, &((struct sockaddr_in *)&ioctlInterface.ifr_netmask)->sin_addr);
+			if (ret == 0) {
+				fprintf(stderr, "Unknown argument: %s\n", argumentName);
+				ret = 1;
+			}
+			else {
+				ret = ioctl(sd, SIOCSIFADDR, &ioctlInterface);
+				if (ret < 0) {
+					perror("ioctl(SIOCGIFFLAGS)");
+				}
+			}
+		}
+		if (ret != 0) {
+			break;
+		}
+	}
 	return ret;
 }
 
@@ -199,7 +448,7 @@ static int psh_ifconfig(int argc, char **argv)
 {
 	const char *interfaceName = NULL;
 	unsigned int flags = 0;
-	int opt, ret;
+	int opt, ret, sd;
 
 	while ((opt = getopt(argc, argv, "va")) != -1) {
 		switch (opt) {
@@ -219,23 +468,32 @@ static int psh_ifconfig(int argc, char **argv)
 		opt += 1;
 	}
 
+	sd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sd < 0) {
+		perror("socket");
+		return EXIT_FAILURE;
+	}
+
 	if ((opt >= argc) || ((flags & IFCONFIG_ALL) != 0)) {
 		/* Ignore options, only display */
-		ret = psh_ifconfigDisplay(flags, interfaceName);
+		ret = psh_ifconfigDisplay(flags, interfaceName, sd);
 		if (ret != 0) {
-			fprintf(stderr, "%s: error fetching interface information: %d", (interfaceName == NULL) ? "" : interfaceName, ret);
-			return EXIT_FAILURE;
+			fprintf(stderr, "%s: error fetching interface information: %d\n", (interfaceName == NULL) ? "" : interfaceName, ret);
+			ret = EXIT_FAILURE;
 		}
 	}
 	else {
-		/* Read options */
-		// TODO
-		for (; opt < argc; opt++) {
-			printf("Non-option argument %s\n", argv[opt]);
+		if (strcmp(argv[opt], "inet") == 0) {
+			opt += 1;
+		}
+		ret = psh_ifconfigHandleArguments(interfaceName, argc, argv, opt, sd);
+		if (ret != 0) {
+			ret = EXIT_FAILURE;
 		}
 	}
 
-	return EXIT_SUCCESS;
+	close(sd);
+	return ret;
 }
 
 
