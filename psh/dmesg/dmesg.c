32d55957ef6dca471954b1a15ce05893f2b73fb8
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2023-11-23 10:58:30 +0100

    dmesg: Add option to disable printing messages to the console
    JIRA: RTOS-648

--- psh/dmesg/dmesg.c
+++ psh/dmesg/dmesg.c
@@ -17,6 +17,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "../psh.h"
 
@@ -29,38 +31,89 @@ void psh_dmesginfo(void)
 
 static void psh_dmesg_help(const char *prog)
 {
-	printf("Usage: %s [options] [files]\n", prog);
+	printf("Usage: %s [options]\n", prog);
+	printf("  -D:  disable the printing of messages to the console\n");
+	printf("  -E:  enable the printing of messages to the console\n");
 	printf("  -h:  shows this help message\n");
+}
+
+
+static int psh_kmsgctrl(bool state)
+{
+	int fd = open("devfs/kmsgctrl", O_WRONLY);
+	if (fd < 0) {
+		fd = open("/dev/kmsgctrl", O_WRONLY);
+	}
+
+	if (fd < 0) {
+		perror("dmesg: Failed to open kmsgctrl");
+		return EXIT_FAILURE;
+	}
+
+	size_t err = psh_write(fd, state ? "1" : "0", 2);
+
+	close(fd);
+
+	if (err != 2) {
+		perror("dmesg: Write to kmsgctrl failed");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
 
 
 int psh_dmesg(int argc, char **argv)
 {
-	char buf[256];
-	int c, fd;
-	ssize_t n;
+	bool help = false, logDis = false, logEn = false;
 
 	for (;;) {
-		c = getopt(argc, argv, "h");
+		int c = getopt(argc, argv, "hDE");
 		if (c == -1) {
 			break;
 		}
 		switch (c) {
 			case 'h':
+				help = true;
+				break;
+
+			case 'E':
+				logEn = true;
+				break;
+
+			case 'D':
+				logDis = true;
+				break;
+
 			default:
 				psh_dmesg_help(argv[0]);
 				return EXIT_FAILURE;
 		}
 	}
 
-	fd = open(_PATH_KLOG, O_RDONLY | O_NONBLOCK);
+	if (help) {
+		psh_dmesg_help(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	if (logDis || logEn) {
+		if (logDis && logEn) {
+			fprintf(stderr, "dmesg: Invalid options.\n");
+			return EXIT_FAILURE;
+		}
+
+		return psh_kmsgctrl(logEn);
+	}
+
+	int fd = open(_PATH_KLOG, O_RDONLY | O_NONBLOCK);
 	if (fd < 0) {
-		fprintf(stderr, "dmesg: Fail to open %s\n", _PATH_KLOG);
-		return 1;
+		fprintf(stderr, "dmesg: Fail to open %s: %s\n", _PATH_KLOG, strerror(errno));
+		return EXIT_FAILURE;
 	}
 
 	for (;;) {
-		n = read(fd, buf, sizeof(buf));
+		char buf[256];
+		ssize_t n = read(fd, buf, sizeof(buf));
 		if (n <= 0) {
 			if ((n == 0) || (errno == EINTR) || (errno == EPIPE)) {
 				continue;
