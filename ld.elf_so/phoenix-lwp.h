20ee08bb7b75cb1eb260100b1c42260076c10b95
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2024-08-29 11:45:43 +0200

    ld.elf_so: Add RISC-V support
    JIRA: RTOS-664

--- ld.elf_so/phoenix-lwp.h
+++ ld.elf_so/phoenix-lwp.h
@@ -14,7 +14,7 @@
 #define STR_HELPER(a) #a
 #define STR(a)        STR_HELPER(a)
 #define DBG()         debug(__FILE__ ":" STR(__LINE__) "\n")
-// #define DBG()
+/* #define DBG() */
 
 
 extern size_t _rtld_tls_static_space;
@@ -22,6 +22,26 @@ extern size_t _rtld_tls_static_space;
 
 #define _lwp_self gettid
 
+#ifdef __HAVE___LWP_GETTCB_FAST
+
+#if defined(__riscv)
+
+static inline void *__lwp_gettcb_fast(void)
+{
+	void *tcb;
+
+	__asm__ volatile("addi %[tcb], tp, %[offset]"
+	    :	[tcb] "=r" (tcb)
+	    :	[offset] "n" (-sizeof(struct tls_tcb)));
+
+	return tcb;
+}
+
+#endif
+
+#else /*__HAVE___LWP_GETTCB_FAST */
+
+#if defined(__i386__)
 
 static inline void *__lwp_getprivate_fast(void)
 {
@@ -32,16 +52,46 @@ static inline void *__lwp_getprivate_fast(void)
 	return tcb;
 }
 
+#endif
+
+#endif /*__HAVE___LWP_GETTCB_FAST */
+
+
+#ifdef __HAVE___LWP_SETTCB
+
+static inline void __lwp_settcb(void *tcb)
+{
+	void *tcbPtr = __lwp_gettcb_fast();
+#ifdef __HAVE_TLS_VARIANT_I
+	memcpy(tcbPtr, tcb, _rtld_tls_static_space + sizeof(struct tls_tcb));
+#else
+	/* Copy static space and tcb. */
+	memcpy(((char *)tcbPtr) - _rtld_tls_static_space,
+           ((char *)tcb)    - _rtld_tls_static_space,
+           _rtld_tls_static_space + sizeof(struct tls_tcb));
+	/* Update self pointer. */
+	struct tls_tcb *tcb = (struct tls_tcb *)tcbPtr;
+	tcb->tcb_self = tcb;
+#endif
+}
+
+#else /* __HAVE___LWP_SETTCB */
+
 static inline void _lwp_setprivate(void *prv)
 {
 	void *privatePtr = __lwp_getprivate_fast();
-    // Copy static space and tcb.
+#ifdef __HAVE_TLS_VARIANT_I
+	memcpy(privatePtr, prv, _rtld_tls_static_space + sizeof(struct tls_tcb));
+#else
+    /* Copy static space and tcb. */
 	memcpy(((char *)privatePtr) - _rtld_tls_static_space,
            ((char *)prv)        - _rtld_tls_static_space,
            _rtld_tls_static_space + sizeof(struct tls_tcb));
-	// Update self pointer.
+	/* Update self pointer. */
 	struct tls_tcb *tcb = (struct tls_tcb *)privatePtr;
 	tcb->tcb_self = tcb;
 }
+
+#endif /* __HAVE___LWP_SETTCB */
 
 #endif
