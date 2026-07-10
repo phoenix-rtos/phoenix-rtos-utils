60458e52c54e96c9c0296faaad1d4032ec0a1043
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-07-10 15:22:38 +0200

    pshapp: fix waiting for foreground
    YT: DO-861

--- psh/pshapp/pshapp.c
+++ psh/pshapp/pshapp.c
@@ -1568,6 +1568,7 @@ static int psh_run(int exitable, const char *console)
 	char *tmp, *cmd, **argv;
 	int cnt, err, argc, retries;
 	pid_t pgrp;
+	sigset_t mask;
 
 	/* Time for klog to print data from buffer */
 	sleep(1);
@@ -1586,15 +1587,10 @@ static int psh_run(int exitable, const char *console)
 		}
 	}
 
-	/* Wait till we run in foreground */
-	if (tcgetpgrp(STDIN_FILENO) != -1) {
-		for (;;) {
-			pgrp = tcgetpgrp(STDIN_FILENO);
-			if (pgrp == getpgrp()) {
-				break;
-			}
-
-			if (kill(-pgrp, SIGTTIN) == 0) {
+	if (sigprocmask(SIG_BLOCK, NULL, &mask) == 0 && sigismember(&mask, SIGTTIN)) {
+		/* Wait till we run in foreground */
+		while ((pgrp = tcgetpgrp(STDIN_FILENO)) != -1 && pgrp != getpgrp()) {
+			if (kill(0, SIGTTIN) == -1) {
 				break;
 			}
 		}
