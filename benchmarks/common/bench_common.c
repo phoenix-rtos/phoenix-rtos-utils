005e145f825691c19e65af2c292692ff98e5c685
Władysław Młynik <wladyslaw.mlynik@phoenix-rtos.com>
2026-08-11 13:37:17 +0200

    benchmarks: implement time measurement on zynqmp

--- benchmarks/common/bench_common.c
+++ benchmarks/common/bench_common.c
@@ -17,6 +17,7 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <board_config.h>
 
 
 #if defined(__CPU_GR740)
@@ -34,6 +35,27 @@ uint64_t bench_getTime(void)
 	cntr = ((uint64_t)(asr22 & 0xffffffu) << 32) | asr23;
 
 	return cntr;
+}
+
+
+#elif defined(__CPU_ZYNQMP)
+
+
+uint64_t bench_getTime(void)
+{
+	uint64_t ticks = 0;
+	// The "isb" ensures we don't read the timer out of order
+	asm volatile(
+			"isb\n"
+#if defined(ZYNQMP_VIRT)
+			"mrs %0, cntpct_el0\n"
+#else
+			"mrs %0, pmccntr_el0\n"
+#endif
+			: "=r"(ticks)
+			:
+			: "memory");
+	return ticks;
 }
 
 
